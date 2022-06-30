63015557378a92e1f0dcce82344c25ffe317e08a
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2022-06-30 19:56:31 +0200

    Update InterfaceService.java

--- src/Interface/InterfaceService.java
+++ src/Interface/InterfaceService.java
@@ -17,8 +17,8 @@ public interface InterfaceService <T>{ //todo: the name should be ServiceInterfa
      public void add(T entity);
     public ArrayList<T> getAll();
     public ObservableList<T> getTout();
-    public void update(T entity);
-    public void delete (T entity);
+    public void update(T entity); 
+    public void delete (T entity); // it's always better to return a flag true or false to let the user know if all was correct or something got broken
     
     public T getById(int id) throws SQLException;
     
